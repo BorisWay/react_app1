24d6217be0db5dc84c72ed2d1bdaf8adc9afc37c
BorisWay <vatniqueiii@gmail.com>
2022-05-12 00:13:52 +0300

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return <header className={s.header}>
+    <img src="https://www.bozy.com/wp-content/uploads/2020/04/Webp.net-resizeimage-1-1.jpg"></img>
+  </header>
+}
+
+export default Header;
